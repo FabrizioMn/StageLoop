d60e955f6e44da26585d0a30145fcd12b66d5ed0
FabrizioMn <Fabriziomn3@gmail.com>
2026-06-13 03:20:12 -0500

    feat(user): se inicio el cambio de arquitectura pasando vertical slices

---
+++ best-project/src/main/java/example/best_project/user/UserRepository.java
@@ -0,0 +1,12 @@
+package example.best_project.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
