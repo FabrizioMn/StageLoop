a8a428bad4823acc5e95fcbf15bfadeed4cc3db9
Fabriziomn <fabriziomn3@gmail.com>
2026-06-22 15:39:57 -0500

    feat(auth): Se agrego la funcionalidad al login y se refactorizo el proceso de registro

--- best-project/src/main/java/example/best_project/auth/service/AuthService.java
+++ best-project/src/main/java/example/best_project/auth/service/AuthService.java
@@ -1,11 +1,14 @@
 package example.best_project.auth.service;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import example.best_project.auth.domain.Token;
 import example.best_project.auth.domain.Token.TokenType;
+import example.best_project.auth.dto.LoginRequest;
 import example.best_project.auth.dto.RegisterRequest;
 import example.best_project.auth.dto.TokenResponse;
 import example.best_project.auth.repository.TokenRepository;
@@ -18,47 +21,68 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final UserRepository userRepository;
-    private final TokenRepository tokenRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final RoleRepository roleRepository;
-    private final JwtService jwtService;
+        private final UserRepository userRepository;
+        private final TokenRepository tokenRepository;
+        private final PasswordEncoder passwordEncoder;
+        private final RoleRepository roleRepository;
+        private final JwtService jwtService;
+        private final AuthenticationManager authenticationManager;
 
-    public TokenResponse register(RegisterRequest request) {
+        public TokenResponse register(RegisterRequest request) {
 
-        var rolCliente = roleRepository.findByRoleName("Cliente")
-                .orElseThrow(() -> new RuntimeException("El rol Cliente no exite"));
+                var rolCliente = roleRepository.findByRoleName("Cliente")
+                                .orElseThrow(() -> new RuntimeException("El rol Cliente no exite"));
 
-        var user = User.builder()
-                .name(request.name())
-                .email(request.email())
-                .password(passwordEncoder.encode(request.password()))
-                .role(rolCliente)
-                .build();
+                var user = User.builder()
+                                .name(request.name())
+                                .email(request.email())
+                                .password(passwordEncoder.encode(request.password()))
+                                .role(rolCliente)
+                                .build();
 
-        var savedUser = userRepository.save(user);
+                var savedUser = userRepository.save(user);
 
-        UserDetails userDetails = org.springframework.security.core.userdetails.User
-                .withUsername(savedUser.getEmail())
-                .password(savedUser.getPassword())
-                .authorities(savedUser.getRole().getRoleName())
-                .build();
+                UserDetails userDetails = org.springframework.security.core.userdetails.User
+                                .withUsername(savedUser.getEmail())
+                                .password(savedUser.getPassword())
+                                .authorities(savedUser.getRole().getRoleName())
+                                .build();
 
-        var jwtToken = jwtService.generateToken(userDetails);
-        var refreshToken = jwtService.generateRefreshToken(userDetails);
-        saveUserToken(savedUser, jwtToken);
-        return new TokenResponse(jwtToken, refreshToken);
-    }
+                var jwtToken = jwtService.generateToken(userDetails);
+                var refreshToken = jwtService.generateRefreshToken(userDetails);
+                saveUserToken(savedUser, jwtToken);
+                return new TokenResponse(jwtToken, refreshToken);
+        }
 
-    private void saveUserToken(User user, String jwtToken) {
-        var token = Token.builder()
-                .user(user)
-                .token(jwtToken)
-                .tokenType(TokenType.BEARER)
-                .expired(false)
-                .revoked(false)
-                .build();
-        tokenRepository.save(token);
-    }
+        public TokenResponse login(LoginRequest request) {
+                authenticationManager.authenticate(
+                                new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+
+                var user = userRepository.findByEmail(request.email())
+                                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+                UserDetails userDetails = org.springframework.security.core.userdetails.User
+                                .withUsername(user.getEmail())
+                                .password(user.getPassword())
+                                .authorities(user.getRole().getRoleName())
+                                .build();
+
+                var jwtToken = jwtService.generateToken(userDetails);
+                var refreshToken = jwtService.generateRefreshToken(userDetails);
+                saveUserToken(user, jwtToken);
+                return new TokenResponse(jwtToken, refreshToken);
+
+        }
+
+        private void saveUserToken(User user, String jwtToken) {
+                var token = Token.builder()
+                                .user(user)
+                                .token(jwtToken)
+                                .tokenType(TokenType.BEARER)
+                                .expired(false)
+                                .revoked(false)
+                                .build();
+                tokenRepository.save(token);
+        }
 
 }
